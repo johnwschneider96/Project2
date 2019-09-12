037456136bc8c9c578aa0884199c4c9a85da2019
John <johnwschneider96@gmail.com>
2019-09-12 13:44:40 -0400

    added controllers

--- P2FB_Application/src/main/java/com/p2/controller/PostController.java
+++ P2FB_Application/src/main/java/com/p2/controller/PostController.java
@@ -1,5 +1,42 @@
 package com.p2.controller;
 
-public class PostController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.p2.model.Post;
+import com.p2.model.User;
+import com.p2.service.PostServImpl;
+
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
+public class PostController
+{
+	@Autowired
+	PostServImpl postServ;
+	
+	@PostMapping(value="/insertpost")
+	public void insertPost(@RequestParam("new_post") Post p) {
+		postServ.insert(p);
+	}
+	
+	@PostMapping(value="/updatepost")
+	public void updatePost(@RequestParam("post") Post p) {
+		postServ.update(p);
+	}
+	
+	@PostMapping(value="/userposts")
+	public @ResponseBody List<Post> selectUserPosts(@RequestParam("user") User u) {
+		return postServ.selectAllByUser(u);
+	}
+	
+	@PostMapping(value="/allposts")
+	public @ResponseBody List<Post> selectAllPosts() {
+		return postServ.selectAll();
+	}
 }
--- P2FB_Application/src/main/java/com/p2/controller/UserController.java
+++ P2FB_Application/src/main/java/com/p2/controller/UserController.java
@@ -1,5 +1,41 @@
 package com.p2.controller;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.p2.model.User;
+import com.p2.service.UserServImpl;
+
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
+public class UserController
+{
+	@Autowired
+	UserServImpl userServ;
+	
+	@PostMapping(value="/insertuser")
+	public void insertPost(@RequestParam("new_user") User u) {
+		userServ.insertUser(u);
+	}
+	
+	@PostMapping(value="/updateuser")
+	public void updatePost(@RequestParam("user") User u) {
+		userServ.updateUser(u);
+	}
+	
+	@PostMapping(value="/getuserbyemail")
+	public @ResponseBody User getUserByEmail(@RequestParam("email") String email) {
+		return userServ.selectByEmailUser(email);
+	}
+	
+	@PostMapping(value="/getallusers")
+	public @ResponseBody List<User> getAllUsers() {
+		return userServ.selectAllUsers();
+	}
 }
