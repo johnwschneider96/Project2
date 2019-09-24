8a8df2fde38d5154f61f2ec6dc40bb58ae2bb78d
lbcarson4 <lbcarson4@gmail.com>
2019-09-23 22:07:29 -0400

    final push

--- src/app/story.ts
+++
@@ -1,6 +0,0 @@
-import { User } from './user';
-
-export class Story {
-    constructor(public postId, public content: string, public numLikes: number, public userEmail: User) {
-    }
-}
--- src/app/user.ts
+++
@@ -1,5 +0,0 @@
-export class User {
-    constructor(public email: string, public password: string, public filename: string, public firstname: string,
-                public lastname: string, public phonenumber: string) {
-    }
-}
